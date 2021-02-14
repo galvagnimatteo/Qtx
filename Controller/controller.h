b8233d8fb0521f475a0f5950d683be627d46857f
Matteo Galvagni <galvigalva@gmail.com>
2021-02-14 11:07:45 +0100

    finished

--- Controller/controller.h
+++ Controller/controller.h
@@ -21,11 +21,6 @@ public:
 
     MainModel* getModel() const;
     View* getView() const;
-    //Nelle classi che ereditano da Controller non sarà possibile fare la forward declaration della propria relativa View
-    //includendo l'informazione che tale view eredita da View, dunque non sarà possibile ritornare un tipo (esempio)
-    //ObliqueView* con getView() perchè ObliqueView sarà definita in modo non completo senza poter includere che deriva da View, quindi non
-    //risulterà figlia di View e l'override non funzionerà.
-    //Per questo, ogni "figlio" di controller userà la getView di View ed eventualmente farà il dynamic_cast al momento.
 
 };
 
